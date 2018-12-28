6742a02e5866299ebac37c5ffd3f6eb06e4531a6
sangx2 <sangrae-lee@naver.com>
2018-12-28 14:20:05 +0900

    파일 코드 변경

---
+++ util/file.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"io/ioutil"
+	"os"
+)
+
+// IsExistFile : 파일 존재 유무 확인
+func IsExistFile(name string) bool {
+	if _, e := os.Stat(name); os.IsNotExist(e) {
+		return false
+	}
+	return true
+}
+
+// GetFileNameList : 해당 경로의 파일 목록 반환
+func GetFileNameList(path string) ([]string, error) {
+	var fileNameList []string
+	files, e := ioutil.ReadDir(path)
+	if e != nil {
+		return nil, e
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			retFileNameList, e := GetFileNameList(path + "/" + file.Name())
+			if e != nil {
+				return nil, e
+			}
+			for _, fileName := range retFileNameList {
+				fileNameList = append(fileNameList, fileName)
+			}
+		} else {
+			fileNameList = append(fileNameList, path+"/"+file.Name())
+		}
+	}
+
+	return fileNameList, nil
+}
